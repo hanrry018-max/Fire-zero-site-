c983c9a5227b3cb6c5afbb87a5db8d9b15836d03
hanrry018-max <hanrry018@gmail.com>
2025-11-26 06:55:01 -0300

    Criar src/app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIRE ZERO</h1>
+      <p>Site em construção</p>
+    </div>
+  )
+}
+
+export default App
